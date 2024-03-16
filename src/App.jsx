232720ceb1b556bb75d24171c2b13a3e253e377a
Varun Mohankumar <varunmj@users.noreply.github.com>
2024-03-15 20:50:08 -0500

    verified

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,28 @@ import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
 
 const App = () => {
-  return <div>
+  return (<div>
     <section id="Homepage">
       <Navbar />
       <Hero />
     </section>
-    <section id="Services"><Parallax type="services" /></section>
-    <section><Services /></section>
-    <section id="Portfolio"><Parallax type="portfolio" /></section>
+    <section 
+      id="Services">
+        <Parallax type="services" />
+    </section>
+    <section>
+      <Services />
+    </section>
+    <section 
+      id="Portfolio">
+        <Parallax type="portfolio" />
+    </section>
     <section >Portfolio1</section>
     <section >Portfolio2</section>
     <section >Portfolio3</section>
     <section id="Contact">Contact</section>
-  </div>;
+  </div>
+  )
 };
 
 export default App;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,35 @@
 import "./services.scss"
+import {motion} from "framer-motion"
 
 const Services = () => {
   return (
-    <div className="services">Services</div>
+    <motion.div className="services">
+      <motion.div className ="textContainer">
+        <p>
+          I work with technology to create 
+          <br /> a change in people's life
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className ="titleContainer">
+        <div className="title">
+          <img src="/people.webp" alt="" />
+          <h1>
+            <b>Unique</b> Ideas
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <b>For Your</b> Business.
+          </h1>
+          <Button>WHAT I'M DOING NOW..</Button>
+        </div>
+      </motion.div>
+      <motion.div className ="listContainer">
+        <div className="box"></div>
+      </motion.div>
+
+    </motion.div>
   )
 }
 
